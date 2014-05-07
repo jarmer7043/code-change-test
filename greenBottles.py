cf55e693d01b47cb5385f71cee611640143a2f13
jarmer7043 <jarmer7043@sohamcollege.org.uk>
2014-05-07 09:41:20 +0100

    Updated Green Bottles
    Added a for loop so it can repeat.

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,12 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+bottles = 10
+for x in range(0,bottles):
+    print("{0} green bottles, hanging on the wall".format(bottles))
+    time.sleep(1.5)
+    print("{0} green bottles, hanging on the wall".format(bottles))
+    time.sleep(1.5)
+    print("And if 1 green bottle should acidentally fall,")
+    time.sleep(1.5)
+    print("There'll be {0} green bottles hanging on the wall.".format(bottles-1))
+    time.sleep(1.5)
+    bottles = bottles-1
