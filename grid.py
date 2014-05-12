1ff3ee62418556308bbae90897f43c36031ed34d
jarmer7043 <jarmer7043@sohamcollege.org.uk>
2014-05-12 17:32:48 +0100

    grid
    fully functioning grid - enter any number of columns or rows

--- grid.py
+++ grid.py
@@ -1,4 +1,10 @@
-print("-------")
-print("| | | |")
-print("-------")
-print("| | | |")
+def grid():
+    numCol = int(input("How many columns would you like?"))
+    numRow = int(input("How many rows would you like?"))
+    col = "| " * (numCol + 1)
+    dashes = "-" * ((numCol * 2) + 1)
+    print(dashes)
+    for x in range(numRow):
+        print(col)
+        print(dashes)
+grid()
